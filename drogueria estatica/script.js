dc3eac1b66993f4f716c1b0e886484b6c2f90c08
Antonio Yecid Gonzalez Garcia <anyeltk@gmail.com>
2025-02-25 20:50:40 -0500

    Add files via upload

---
+++ drogueria estatica/script.js
@@ -0,0 +1,12 @@
+<script>
+  document.addEventListener("DOMContentLoaded", function () {
+    const cruces = document.querySelectorAll(".cruz");
+
+    cruces.forEach((cruz) => {
+      const randomTop = Math.random() * 120 - 10; // Rango: -10% a 110%
+      const randomLeft = Math.random() * 120 - 10; // Rango: -10% a 110%
+      cruz.style.top = `${randomTop}%`;
+      cruz.style.left = `${randomLeft}%`;
+    });
+  });
+</script>
