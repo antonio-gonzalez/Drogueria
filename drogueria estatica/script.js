1b9588a2b5e122605a5f9e5b76c4d3db961a6af1
Antonio Yecid Gonzalez Garcia <anyeltk@gmail.com>
2025-02-25 22:55:48 -0500

    Update script.js

--- drogueria estatica/script.js
+++ drogueria estatica/script.js
@@ -2,7 +2,7 @@
   document.addEventListener("DOMContentLoaded", function () {
     const cruces = document.querySelectorAll(".cruz");
 
-    cruces.forEach((cruz) => {
+    cruces.forEach((cruz) = {
       const randomTop = Math.random() * 120 - 10; // Rango: -10% a 110%
       const randomLeft = Math.random() * 120 - 10; // Rango: -10% a 110%
       cruz.style.top = `${randomTop}%`;
